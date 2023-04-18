c2e98e9f1a25d6e54ed65eb72c410829f6fd9526
petrostrak <pit.trak@gmail.com>
2023-04-18 22:43:33 +0300

    Update healthcheck handler to use writeJSON helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, data, nil); err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Could not write to json", http.StatusInternalServerError)
+	}
 }
