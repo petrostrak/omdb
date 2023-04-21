494db1e76ea8b43449e0f64e24d3e6c072e7eebb
petrostrak <pit.trak@gmail.com>
2023-04-21 19:14:13 +0300

    Add methodNotAllowedResponse method

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -25,7 +25,12 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
-func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
