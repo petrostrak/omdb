67b01580ad3304d8445200538a79257a6e8d16e1
petrostrak <pit.trak@gmail.com>
2023-04-26 13:03:03 +0300

    Add validation methods Valid, AddError and Check

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -11,3 +11,21 @@ type Validator struct {
 func New() *Validator {
 	return &Validator{Errors: make(map[string]string)}
 }
+
+// Valid returns true if the errors map doesn't contain any entries.
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+// Check adds an error message to the map only if a validation check is not ok.
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
