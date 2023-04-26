8d5f2e7daf12cd9c095d58395a643370e6d18fe7
petrostrak <pit.trak@gmail.com>
2023-04-26 12:49:27 +0300

    Customize json decoding for runtime field

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -12,10 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// Declare an anonymous struct to hold the info that we expect to be in the HTTP
 	// request body.
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtume int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtume data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -11,4 +15,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(json []byte) error {
+	unquotedJSON, err := strconv.Unquote(string(json))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Isolate the part containing the number
+	parts := strings.Split(unquotedJSON, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
