3a16724a1f966dbc830b51cf0a3e58de2343944d
petrostrak <pit.trak@gmail.com>
2023-05-10 13:04:43 +0300

    Update routes to include update handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,6 +16,7 @@ func (app *application) routes() http.Handler {
 		r.Route("/movies", func(r chi.Router) {
 			r.Post("/", app.createMovieHandler)
 			r.Get("/{id}", app.showMovieHandler)
+			r.Put("/{id}", app.updateMovieHandler)
 		})
 	})
 
