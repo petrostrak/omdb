0bf61a21bc84e04631b6fa3d82995396ceee38ba
petrostrak <pit.trak@gmail.com>
2023-05-11 12:45:52 +0300

    Update routes to include delete handler

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -17,6 +17,7 @@ func (app *application) routes() http.Handler {
 			r.Post("/", app.createMovieHandler)
 			r.Get("/{id}", app.showMovieHandler)
 			r.Put("/{id}", app.updateMovieHandler)
+			r.Delete("/{id}", app.deleteMovieHandler)
 		})
 	})
 
